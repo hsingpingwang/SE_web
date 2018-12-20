23ba03ee41463f94aaec9076018c10c30a15da41
ChinYa <smallrespect33@gmail.com>
2018-12-20 14:52:15 +0800

    add template
    但部分跳轉的功能 可能要幫我看一下

--- application/view/home.php
+++ application/view/home.php
@@ -4,8 +4,8 @@
     include '../controller/HeWoController.php';
 
     session_start();
-    echo $_SESSION['model']->account;
-    echo $_SESSION['model']->password;
+    // echo $_SESSION['model']->account;
+    // echo $_SESSION['model']->password;
     if (isset($_POST['FriendBtn'])) {
         $_SESSION['controller']->userHasInput($_POST['FriendBtn']);
     }
@@ -24,18 +24,204 @@
     $_SESSION['model']->initState("HOME");
 ?>
 
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    這裡是主畫面!
-    <form action="" method="POST">
-      <input type="submit" name="FriendBtn" value="Friend" >
-      <input type="submit" name="FamilyBtn" value="Family" >
-      <input type="submit" name="CardBtn" value="Card" >
-      <input type="submit" name="HomeBtn" value="Home" >
-      <input type="submit" name="LogoutBtn" value="Logout" >
-    </form>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hello World</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="vendor/css/clean-blog.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="home.php">Hello World</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <form action="" method="POST">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <!--<input class="nav-link" type="submit" name="HomeBtn" value="Home" >-->
+              <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <!--<input class="nav-link" type="submit" name="FriendBtn" value="Friend" >-->
+              <a class="nav-link" href="profile.php">Profile</a>
+            </li>
+            <li class="nav-item">
+              <!--<input class="nav-link" type="submit" name="CardBtn" value="Card" >-->
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+            <li class="nav-item">
+              <!--<input class="nav-link" type="submit" name="FriendBtn" value="Friend" >-->
+              <a class="nav-link" href="Friend.View.php">Friend</a>
+            </li>
+            <li class="nav-item">
+              <!--<input class="nav-link" type="submit" name="FamilyBtn" value="Family" >-->
+              <a class="nav-link" href="Family.View.php">Family</a>
+            </li>
+            <li class="nav-item">
+              <!--<input class="nav-link" type="submit" name="CardBtn" value="Card" >-->
+              <a class="nav-link" href="Card.View.php">Card</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Logout</a>
+              <!--<input class="nav-link" type="submit" name="LogoutBtn" value="Logout" >-->
+            </li>
+          </ul>
+        </form>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('vendor/img/home-bg.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>Clean Blog</h1>
+              <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview">
+            <a href="post.php">
+              <h2 class="post-title">
+                Man must explore, and this is exploration at its greatest
+              </h2>
+              <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 24, 2018</p>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.php">
+              <h2 class="post-title">
+                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+              </h2>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 18, 2018</p>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.php">
+              <h2 class="post-title">
+                Science has not yet mastered prophecy
+              </h2>
+              <h3 class="post-subtitle">
+                We predict too much for the next year and yet far too little for the next ten.
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on August 24, 2018</p>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.php">
+              <h2 class="post-title">
+                Failure is not an option
+              </h2>
+              <h3 class="post-subtitle">
+                Many say exploration is part of our destiny, but it’s actually our duty to future generations.
+              </h3>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on July 8, 2018</p>
+          </div>
+          <hr>
+          <!-- Pager -->
+          <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                  </span>
+                </a>
+              </li>
+            </ul>
+            <p class="copyright text-muted">Copyright &copy; Your Website 2018</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="vendor/js/clean-blog.min.js"></script>
+
+  </body>
+
 </html>
--- application/view/index.php
+++ application/view/index.php
@@ -24,13 +24,101 @@
     $_SESSION['model']->initState("START");
 
 ?>
+<!DOCTYPE HTML>
+    <html><head>
+        <title>Classic Sign In Form Responsive Widget Template :: w3layouts</title>
+        <!-- Meta tag Keywords -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="Classic Sign In Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements"
+        />
+        <script>
+            addEventListener("load", function () {
+                setTimeout(hideURLbar, 0);
+            }, false);
 
-<html>
-<body>
+            function hideURLbar() {
+                window.scrollTo(0, 1);
+            }
+        </script>
+        <!-- Meta tag Keywords -->
+        <!-- css files -->
+        <link rel="stylesheet" href="vendor/css/style.css" type="text/css" media="all" />
+        <!-- Style-CSS -->
+        <link rel="stylesheet" href="vendor/css/fontawesome-all.css">
+        <!-- Font-Awesome-Icons-CSS -->
+        <!-- //css files -->
+        <!-- web-fonts -->
+        <link href="//fonts.googleapis.com/css?family=Reem+Kufi&amp;subset=arabic" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <!-- //web-fonts -->
+    </head>
+    <body>
+    <!-- title -->
+    <h1>
+        <span>H</span>ello <span>W</span>orld!
+    </h1>
+    <!-- //title -->
+    <!-- content -->
+    <div class="sub-main-w3">
+        <form action="#" method="post">
+            <div class="form-style-agile">
+                <label>
+                    Username
+                    <i class="fas fa-user"></i>
+                </label>
+                <input placeholder="Username" name="username" type="text" >
+            </div>
+            <div class="form-style-agile">
+                <label>
+                    Password
+                    <i class="fas fa-unlock-alt"></i>
+                </label>
+                <input placeholder="Password" name="password" type="password">
+            </div>
+            <!-- checkbox -->
+            <div class="wthree-text">
+                <ul>
+                    <li>
+                        <!-- switch -->
+                        <div class="checkout-w3l">
+                            <div class="demo5">
+                                <div class="switch demo3">
+                                    <input type="checkbox">
+                                    <label>
+                                        <i></i>
+                                    </label>
+                                </div>
+                            </div>
+                            <p>&nbsp Stay Signed In</p>
+                        </div>
+                        <!-- //checkout -->
+                    </li>
+                    <li>
+                        <a href="">Forgot Password?</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- //switch -->
+            <input type="submit" name="LoginBtn" value="Login" >
+        </form>
+    </div>
+    <!-- //content -->
+
+    <!-- copyright -->
+    <div class="footer">
+        <p>&copy; 2018 Hello World social media.| Design by
+            <a href="http://google.com">HeWo</a>
+        </p>
+    </div>
+    <!-- //copyright -->
+
+</body>
+<!--<body>
     <form action="" method="POST">
         帳號：<input name="username" type="text" /><br/>
         密碼：<input name="password" type="password" /><br/>
       <input type="submit" name="LoginBtn" value="Login" >
     </form>
-</body>
+</body>-->
 </html>
